c43b5f724c42036e749489069357e65efde17ad9
NH118742_hertz <r.meitei@hertz.com>
2023-10-23 16:04:13 -0400

    touch up nagivation

--- components/DesktopNavigation.tsx
+++ components/DesktopNavigation.tsx
@@ -18,13 +18,13 @@ function NavItem({
         className={clsx(
           "relative block px-3 py-2 transition",
           isActive
-            ? "text-teal-500 dark:text-teal-400"
-            : "hover:text-teal-500 dark:hover:text-teal-400"
+            ? "text-blue-500 dark:text-blue-400"
+            : "hover:text-teal-500 dark:hover:text-green-400"
         )}
       >
         {children}
         {isActive && (
-          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-blue-400/40 dark:to-teal-400/0" />
         )}
       </Link>
     </li>
